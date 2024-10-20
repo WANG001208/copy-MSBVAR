a674f856d3c6496287c575fc07adb77da7b48478
WANG001208 <63024815+WANG001208@users.noreply.github.com>
2024-10-20 23:29:03 +0800

    Update msvar.R

--- R/msvar.R
+++ R/msvar.R
@@ -130,13 +130,14 @@ msvar <- function(Y, p, h, niterblkopt=10)
 
     # param_opt里面的参数顺序，取决于llf.msvar的需求
     # 因为原始的llf.msar比较奇怪，这里把llf.msar也改了一下，加上optstr="all"，并且
-    param_opt <- array(NA, c(1+m*p+m, m, h))
+    param_opt <- array(NA, c(1+m*p+m+m, m, h))
     param_opt[1:m*p,,] <- output$hreg$Bk[1:m*p,,]
     param_opt[m*p+1,,] <- output$hreg$Bk[1+m*p,,]
     # 这里需要注意，在R的index里面，:比+有更高的优先计算级，所以当我们的index设计加法运算时，必须加上括号
     print(param_opt[(2+m*p):(m+m*p+1),,])
     print(output$Q)
     param_opt[(2+m*p):(m+m*p+1),,] <- output$hreg$Sigmak
+    param_opt[(m*p+m+2):(m*p+m+m+1),,] <- output$Q,c(2,1)
 
     output_theta <- array(NA, c((1+m*p+m),m,h))
     print(output_theta[1:(m*p+1),,])
@@ -250,7 +251,7 @@ llf_msar <- function(param_opt, Y, X, p, theta, Q, optstr, ms.switch) {
     Qhat <- cbind(Qhat, 1-rowSums(Qhat))
   } else if (optstr=='all'){
     # passing all the estimation in param_opt
-    Qhat <- matrix(param_opt[(2+m*p):(m+m*p+1-1),,], nrow=h, ncol=h-1)
+    Qhat <- matrix(param_opt[(2+m*p+m):(m+m+m*p+1-1),,], nrow=h, ncol=h-1)
     Qhat <- cbing(Qhat, 1-rowSums(Qhat))
 
     beta0 <- array(param_opt[(m*p+1),,],c(m,1,h))
