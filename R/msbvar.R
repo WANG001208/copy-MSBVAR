7f9a3ad07cd50fe0406e0464033ce0acf1ed559e
WANG001208 <63024815+WANG001208@users.noreply.github.com>
2024-10-20 16:33:14 +0800

    Update msbvar.R

--- R/msbvar.R
+++ R/msbvar.R
@@ -373,7 +373,7 @@ llf.msar <- function(param.opt, Y, X, p, theta, Q, optstr, ms.switch) {
     Qhat <- cbind(Qhat, 1-rowSums(Qhat))
   } else if (optstr=='all'){
     # passing all the estimation in param.opt
-    Qhat <- matrix(param.opt[2+m*p:m+m*p+1:,:,], nrow=h, ncol=h-1)
+    Qhat <- matrix(param.opt[2+m*p:m+m*p+1,,], nrow=h, ncol=h-1)
     Qhat <- cbing(Qhat, 1-rowSums(Qhat))
 
     beta0 <- array(param.opt[m*p+1,,],c(m,1,h))
