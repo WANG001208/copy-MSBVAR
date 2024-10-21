f4035d00cd7965b3d3bafe13c02406f8606abd6e
WANG001208 <63024815+WANG001208@users.noreply.github.com>
2024-10-21 16:08:07 +0800

    Update msvar.R

--- R/msvar.R
+++ R/msvar.R
@@ -177,7 +177,7 @@ msvar <- function(Y, p, h, niterblkopt=10)
     output_theta = aperm(output_theta, c(2, 1, 3))
     print(param_opt)
     # optim_result <- fdHess(pars=param_opt, fun=llf_msar, Y=Y, X=X, p=p, theta=output_theta,Q=output$Q, optstr='all', ms.switch=indms)$Hessian
-    optim_result <- optim(par=param_opt, fn=llf_msar, gr=NULL, Y=Y, X=X, p=p, theta=output_theta,Q=output$Q, optstr='all', ms.switch=indms,control = list(maxit = 100000),hessian=TRUE)
+    optim_result <- optim(par=param_opt, fn=llf_msar, gr=NULL, Y=Y, X=X, p=p, theta=output_theta,Q=output$Q, optstr='all', ms.switch=indms,control = list(maxit = 10),hessian=TRUE)
     print("maxit")
     print(100000)
     
@@ -284,7 +284,7 @@ llf_msar <- function(param_opt, Y, X, p, theta, Q, optstr, ms.switch) {
       
     dim(param_opt) <- c(m*(1+m*p+(m+1)/2)+h-1,h)
     # print("param_opt")
-    # print(param_opt)
+    print(param_opt)
     # print(param_opt[(m*(1+m*p+(m+1)/2)+1):(m*(1+m*p+(m+1)/2)+h-1),])
     Q_hat <- array(NA,c(h-1,h))
     Qhat <- param_opt[(m*(1+m*p+(m+1)/2)+1):(m*(1+m*p+(m+1)/2)+h-1),]
