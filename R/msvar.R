32e1827dea64de673462e504ea36da36c19c40d9
WANG001208 <63024815+WANG001208@users.noreply.github.com>
2024-10-21 15:23:53 +0800

    Update msvar.R

--- R/msvar.R
+++ R/msvar.R
@@ -175,14 +175,17 @@ msvar <- function(Y, p, h, niterblkopt=10)
 
     output_theta = aperm(output_theta, c(2, 1, 3))
     # optim_result <- fdHess(pars=param_opt, fun=llf_msar, Y=Y, X=X, p=p, theta=output_theta,Q=output$Q, optstr='all', ms.switch=indms)$Hessian
-    optim_result <- optim(par=param_opt, fn=llf_msar, gr=NULL, Y=Y, X=X, p=p, theta=output_theta,Q=output$Q, optstr='all', ms.switch=indms,control = list(maxit = 100000),hessian=TRUE)$hessian
+    optim_result <- optim(par=param_opt, fn=llf_msar, gr=NULL, Y=Y, X=X, p=p, theta=output_theta,Q=output$Q, optstr='all', ms.switch=indms,control = list(maxit = 100000),hessian=TRUE)
+    print("maxit")
+    print(100000)
     
     print("got optim_result")
     print(optim_result)
-    std <- sqrt(abs(diag(solve(optim_result))))
-    print("got std")
-    print(std)
-    output$param_std <- std
+    # std <- sqrt(abs(diag(solve(optim_result))))
+    # print("got std")
+    # print(std)
+    # output$param_std <- std
+    output$hessian <- optim_result
     class(output) <- "MSVAR"
 
 return(output)
